c3be316f1783e44495c54ff120c11bab809dc99a
Fred Mansfield <fredgybrah@gmail.com>
2025-07-15 12:04:54 -0400

    completed stage02 add getCustomer

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const path = require('path');  // for handling file paths
 const app = express();
 const port = process.env.PORT || 4000;  // use env var or default to 4000
 
+const da = require("./data-access.js");  // import data access module
+
 // Set the static directory to serve files from
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
+});
+
+app.get("/customers", async (req, res) => {
+  const cust = await da.getCustomers();
+  res.send(cust);
 });
